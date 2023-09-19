76b2c6f8a7c33a157565309ddc793dc6caa2ca51
Kim Jiwon <stayby16@kakao.com>
2023-09-19 23:33:51 +0900

    css 추가

--- pages/user/findID.tsx
+++ pages/user/findID.tsx
@@ -1,5 +1,5 @@
 /* eslint-disable */
-'use client'
+"use client";
 
 import Link from "next/link";
 import React, { useState } from "react";
@@ -23,20 +23,27 @@ export default function findUserId() {
 
   return (
     <>
-      <div>
-        <form onSubmit={onSubmit}>
+      <div className="flex flex-col items-center justify-center h-screen w-screen">
+        <form onSubmit={onSubmit} className="flex items-center justify-center">
           <input
+            className="text-2xl font-bold mt-6 shadow-md h-12 hover:scale-110"
             type="text"
             placeholder="이메일을 입력하세요"
             value={email}
             onChange={onChange}
           />
-          <button>찾기</button>
+          <button className="text-2xl ml-5 font-bold mt-6 shadow-md h-12 bg-orange-300 rounded-md px-5 hover:bg-orange-600 hover:scale-110 active:bg-yellow-400 transition-all">
+            찾기
+          </button>
         </form>
-        <div>
-          <span>{findUserId}</span>
+        <div className="mt-6">
+          <span className="text-red-500 font-bold text-2xl mb-6">
+            {findUserId}
+          </span>
         </div>
-        <Link href="/user/login">로그인 페이지로</Link>
+        <Link href="/user/login">
+          <span className="font-bold text-lg">로그인 페이지로</span>
+        </Link>
       </div>
     </>
   );
--- pages/user/myinfo.tsx
+++ pages/user/myinfo.tsx
@@ -1,5 +1,5 @@
 /* eslint-disable */
-'use client'
+"use client";
 
 import { useEffect, useState } from "react";
 import { getMyInfo, getMyPage, modifierUser } from "../api/myinfo";
@@ -44,67 +44,86 @@ export default function MyInfo() {
   };
   return (
     <>
-      <div>
-        <div>
-          <div>로고</div>
-          <span>전국 포인트</span>
-          <span>나만의 포인트</span>
-          <span>커뮤니티</span>
-          <br />
-          <span>{data?.userId}</span>
-          <button className="bg-gray-500">로그아웃</button>
+      <div className="flex flex-col items-center justify-start w-screen h-screen mt-6">
+        <div className="w-40 bg-slate-200 h-40 text-center shadow-lg rounded-xl">
+          로고
         </div>
-        <div>
-          <div>
-            <div>기본이미지</div>
-          </div>
-          <div>
-            <span>닉네임</span>
-            <span>{data?.nickname}</span>
-            <button onClick={modifierBtn} className="bg-gray-500">
+        <div className="flex justify-around items-center font-semibold mt-10 w-6/12 text-lg">
+          <span className="transition-all cursor-pointer hover:scale-105 mx-1 hover:text-blue-300">
+            전국 포인트
+          </span>
+          <span className="transition-all cursor-pointer hover:scale-105 mx-1 hover:text-blue-300">
+            나만의 포인트
+          </span>
+          <span className="transition-all cursor-pointer hover:scale-105 mx-1 hover:text-blue-300">
+            커뮤니티
+          </span>
+          <span className="transition-all hover:scale-105 mx-1 hover:text-blue-300">
+            환영합니다.&nbsp;&nbsp;{data?.userId}님
+          </span>
+          <button className="text-xl w-2/12 font-bold shadow-md h-10 bg-orange-300 rounded-md hover:bg-orange-600 hover:scale-110 active:bg-yellow-400 transition-all">
+            로그아웃
+          </button>
+        </div>
+        <div className="flex items-center justify-center">
+          <img className="w-20 bg-slate-200 h-20 text-center shadow-lg rounded-xl mt-6" />
+          <div className="font-semibold text-lg mt-6 ml-4 space-x-5">
+            <span>{data?.nickname}&nbsp;님</span>
+            <button
+              onClick={modifierBtn}
+              className="text-lg w-32 font-bold shadow-md h-10 bg-orange-300 rounded-md hover:bg-orange-600 hover:scale-110 active:bg-yellow-400 transition-all"
+            >
               회원수정
             </button>
-            <button onClick={resignBtn} className="bg-red-500">
+            <button
+              onClick={resignBtn}
+              className="text-lg w-20 font-bold shadow-md h-10 bg-red-400 rounded-md hover:bg-red-600 hover:scale-110 active:bg-red-800 transition-all"
+            >
               탈퇴
             </button>
           </div>
         </div>
-        <div className="mt-5">
-          <div className="grid grid-cols-1 grid-rows-2 bg-orange-300">
-            <div className="flex justify-between">
+        <div className="mt-20 w-11/12">
+          <div className="container mx-auto flex justify-center items-center flex-col">
+            <div className="flex justify-around w-8/12 border border-gray-300 shadow-lg">
               <span>제목</span>
               <span>작성자</span>
               <span>등록일</span>
               <span>좋아요</span>
             </div>
-            <div>
+            <div className="shadow-lg w-8/12">
               {page.map((item: any) => {
                 return (
-                  <div key={item.id} className="flex justify-between">
-                    <span>{item.contents}</span>
-                    <span>{item.accountId}</span>
-                    <span>{item.createdTime}</span>
-                    <span>{item.postLike}</span>
+                  <div
+                    key={item.id}
+                    className="flex justify-around px-10 py-4 font-medium text-gray-900 whitespace-nowrap"
+                  >
+                    <span className="px-6 py-4">{item.contents}</span>
+                    <span className="px-6 py-4">{item.accountId}</span>
+                    <span className="px-6 py-4">{item.createdTime}</span>
+                    <span className="px-6 py-4">{item.postLike}</span>
                   </div>
                 );
               })}
             </div>
           </div>
-          <div className="mt-5 grid-cols-1 grid-rows-2 bg-slate-400">
-            <div>등록한 포인트</div>
-            <div>
-              <div className="flex justify-between">
+          <div>
+            <div className="container mx-auto flex justify-center items-center flex-col">
+              <div className="mt-10 flex justify-around w-8/12 border border-gray-300 shadow-lg">
                 <span>이름</span>
                 <span>위치</span>
                 <span>등록일</span>
               </div>
-              <div>
+              <div className="shadow-lg w-8/12">
                 {page.map((item: any) => {
                   return (
-                    <div key={item.id} className="flex justify-between">
-                      <span>{item.title}</span>
-                      <span>{item.locationdate}</span>
-                      <span>{item.createdTime}</span>
+                    <div
+                      key={item.id}
+                      className="flex justify-around px-10 py-4 font-medium text-gray-900 whitespace-nowrap"
+                    >
+                      <span className="px-6 py-4">{item.title}</span>
+                      <span className="px-6 py-4">{item.locationdate}</span>
+                      <span className="px-6 py-4">{item.createdTime}</span>
                     </div>
                   );
                 })}
